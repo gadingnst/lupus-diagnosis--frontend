f7bc9863259c02fdfd69899a180dccbf76179072
Gading <sutan.gnst@gmail.com>
2020-07-06 23:16:03 +0700

    prediction done

--- src/screens/Result/index.tsx
+++ src/screens/Result/index.tsx
@@ -1,8 +1,12 @@
-import { PureComponent } from 'react'
-import { View, Text } from 'react-native'
+import { PureComponent, ReactNode } from 'react'
+import { View, Text, Button } from 'react-native'
+import { CommonActions } from '@react-navigation/native'
 import { StackScreenProps } from '@react-navigation/stack'
 import { RootStackParamsList } from 'navigator'
 import { PredictApi } from 'api/Case'
+import { ScrollView } from 'react-native-gesture-handler'
+import styles from './styles'
+import { Theme } from 'configs'
 
 export interface ParamsList {
   prediction?: PredictApi
@@ -16,20 +20,49 @@ interface Props extends StackScreenProps<RootStackParamsList, 'Result'> {}
 
 class Result extends PureComponent<Props, State> {
   constructor(props: Props) {
-    const { route } = props
     super(props)
+    const { route } = props
     this.state = {
       data: route.params.prediction
     }
   }
 
-  // TODO
+  public done = (): void => {
+    const { navigation } = this.props
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 1,
+        routes: [{ name: 'Questions' }]
+      })
+    )
+  }
+
+  public renderResult(): ReactNode {
+    const { data } = this.state
+    return data?.classification.map((item, idx) => (
+      <Text key={idx} style={styles.classification}>
+        {item.idName} : {item.percentage}
+      </Text>
+    ))
+  }
+
   public render(): JSX.Element {
     const { data } = this.state
     return (
-      <View>
-        <Text>{JSON.stringify(data)}</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>Hasil Diagnosa</Text>
+        <View style={styles.result}>{this.renderResult()}</View>
+        <Text style={styles.subtitle}>Kesimpulan :</Text>
+        <Text>
+          Anda terindikasi Penyakit
+          <Text style={styles.prediction}>
+            &nbsp;{data?.prediction.idName} / {data?.prediction.name}.
+          </Text>
+        </Text>
+        <View style={styles.btnContainer}>
+          <Button title="Selesai" color={Theme.success} onPress={this.done} />
+        </View>
+      </ScrollView>
     )
   }
 }
--- src/screens/Result/styles.ts
+++ src/screens/Result/styles.ts
@@ -1,3 +1,33 @@
 import { StyleSheet } from 'react-native'
 
-export default StyleSheet.create({})
+export default StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 28,
+    marginTop: 30,
+    textAlign: 'center'
+  },
+  result: {
+    marginTop: 40,
+    width: '100%'
+  },
+  classification: {
+    marginVertical: 8,
+    fontSize: 16
+  },
+  subtitle: {
+    marginVertical: 25,
+    fontSize: 14
+  },
+  prediction: {
+    fontWeight: 'bold'
+  },
+  btnContainer: {
+    marginVertical: 25,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
